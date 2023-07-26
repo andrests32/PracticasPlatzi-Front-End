231f3f3c438b699358f41fe8ac6ff662cb284d32
andrests <andresterraza.at@gmail.com>
2023-07-26 11:23:47 -0500

    Solo hemos realizado pequoños cambios en JS y Html viendo los atributos que tienen cada etiqueta enlazada con javascrip

--- JS basico practico/indexJS.js
+++ JS basico practico/indexJS.js
@@ -1,10 +1,10 @@
 const h1 = document.querySelector(`h1`);
 const p = document.querySelector(`p`);
-const parrafito = document.querySelector(`.parrafito`);
+const parrafito = document.getElementsByClassName(`.parrafito`);
 const pid = document.querySelector(`#pid`);
 const input = document.querySelector(`input`);
 
-console.log(h1)
+console.log(input.value)
 
 console.log({
     h1,
@@ -14,5 +14,16 @@ console.log({
     input,
 });
 
+//Con este tipo de elemento "innerHTML" podemos hacer HTML directamente en JavaScrip y colocar codigo html malicioso, lo que por que seguridad se debe utilizar "innerText" para poder defender nuestro codigo de posibles ataques informaticos..!!
+
+h1.innerHTML = `Patito <br> feo`;
+h1.innerText = `Patito <br> feo`;
 
 
+
+// console.log(h1.getAttribute(`class`));
+// h1.setAttribute(`class`, `rojo`);
+
+///Con estas propiedades podemos modificar las clases de cada elemento que tenga nuestro codigo de html 
+h1.classList.add(`rojo`);
+h1.classList.remove(`verde`);
